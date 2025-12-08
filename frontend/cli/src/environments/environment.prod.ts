76d726d4ae51c195aa2d2b920b54d12022096787
Agustin Palma <aguspalqui@hotmail.com>
2025-12-08 20:10:56 -0300

    feat: agrego configuraciones de despliegue

--- frontend/cli/src/environments/environment.prod.ts
+++ frontend/cli/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // Environment para producción (Netlify)
 export const environment = {
   production: true,
-  apiUrl: 'https://turneroweb.onrender.com' // Backend en Render
+  apiUrl: '168.197.48.210:8080' // Backend en Render
 };
