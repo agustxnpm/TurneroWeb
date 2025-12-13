c7eb32def179df4f3efccadb8754dfacf2a56870
TahielLC <sentey.44@gmail.com>
2025-12-12 21:21:55 -0300

    Encuestas base agregada

--- backend/src/main/java/unpsjb/labprog/backend/business/repository/EncuestaPlantillaRepository.java
+++ backend/src/main/java/unpsjb/labprog/backend/business/repository/EncuestaPlantillaRepository.java
@@ -1,6 +1,7 @@
 package unpsjb.labprog.backend.business.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,7 +13,12 @@ import unpsjb.labprog.backend.model.Especialidad;
 @Repository
 public interface EncuestaPlantillaRepository extends JpaRepository<EncuestaPlantilla, Integer> {
     List<EncuestaPlantilla> findByEspecialidadAndCentroAtencion(Especialidad especialidad, CentroAtencion centro);
+
     List<EncuestaPlantilla> findByCentroAtencion(CentroAtencion centro);
+
     List<EncuestaPlantilla> findByEspecialidad(Especialidad especialidad);
+
     List<EncuestaPlantilla> findByCentroAtencionIsNullAndEspecialidadIsNull();
+
+    Optional<EncuestaPlantilla> findByNombre(String nombre);
 }
