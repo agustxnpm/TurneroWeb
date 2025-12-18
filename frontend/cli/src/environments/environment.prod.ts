686175dfecb989d5a2d730e9f3a4c0d889d2caa2
Agustin Palma <aguspalqui@hotmail.com>
2025-12-18 14:40:22 -0300

    feat: agrego reverse proxy para no exponer backend

--- frontend/cli/src/environments/environment.prod.ts
+++ frontend/cli/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 // Environment para producción (Netlify)
 export const environment = {
   production: true,
-apiUrl: 'http://168.197.48.210:8080'
+  apiUrl: '/api',  // ✅ Usa ruta relativa (Nginx hace el proxy)
+  googleClientId: '792207143081-bjp6agdmp0a7aip4tq0ta35u1j50fuuk.apps.googleusercontent.com'
 };
